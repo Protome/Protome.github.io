3ff2a580b07542834e74a71d6198cbfa739e50e3
Protome <kieran.bamford@gmail.com>
2015-10-24 17:20:43 +0100

    Changes!

--- js/main.js
+++ js/main.js
@@ -1,43 +1,43 @@
-(function() {
-	loadOptions();
-	submitHandler();
-})();
+// (function() {
+// 	loadOptions();
+// 	submitHandler();
+// })();
 
-function submitHandler() {
-		var $submitButton = $('#submitButton');
-		$submitButton.on('click', function() {
-		console.log('Submit');
-		var return_to = getQueryParam('return_to', 'pebblejs://close#');
-		document.location = return_to + encodeURIComponent(JSON.stringify(getAndStoreConfigData()));
-	});
-}
-function loadOptions() {
-	var $backgroundColorPicker = $('#backgroundColourPicker');
-	
-	if (localStorage.backgroundColor) {
-		$backgroundColorPicker[0].value = localStorage.backgroundColour;
-	}
-}
+function getConfigData() {
+    var backgroundColorPicker = document.getElementById('backgroundColourPicker');
+ 
+    var options = {
+      'background_color': backgroundColorPicker.value
+    };
+    // Save for next launch
+    localStorage['backgroundColor'] = options['backgroundColor'];
+    console.log('Got options: ' + JSON.stringify(options));
+    return options;
+  }
+  
+  function getQueryParam(variable, defaultValue) {
+    var query = location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split('=');
+      if (pair[0] === variable) {
+        return decodeURIComponent(pair[1]);
+      }
+    }
+    return defaultValue || false;
+  }
 
-function getAndStoreConfigData() {
-	var $backgroundColorPicker = $('#backgroundColourPicker');
+  var submitButton = document.getElementById('submitButton');
+  submitButton.addEventListener('click', function() {
+    console.log('Submit');
+    // Set the return URL depending on the runtime environment
+    var return_to = getQueryParam('return_to', 'pebblejs://close#');
+    document.location = return_to + encodeURIComponent(JSON.stringify(getConfigData()));
+  });
 	
-	var options = {
-		backgroundColor: $backgroundColorPicker.val()	
-	};
-	
-	localStorage.backgroundColor = options.backgroundColor;
-	return options;
-}
-
-function getQueryParam(variable, defaultValue) {
-	var question = location.search.substring(1);
-	var vars = query.split('&');
-	for (var i = 0; i < vars.length; i++) {
-		var pair = vars[i].split('=');
-		if (pair[0] === variable) {
-			return decodeURIComponent(pair[1]);
-		}
-	}
-	return defaultValue || false;
-}
+	 (function() {
+		 var backgroundColorPicker = document.getElementById('backgroundColourPicker');
+		 if(localStorage['backgroundColor']) {
+      backgroundColorPicker.value = localStorage['backgroundColor'];
+    }
+  })();
